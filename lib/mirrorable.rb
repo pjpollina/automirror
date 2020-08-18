ceb84d4933499e68b881ea80c58d40bc00489599
PJ Pollina <contact@pjpollina.info>
2020-08-18 15:58:31 -0400

    Added private functions set_origin and set_reflection to Mirrorable

--- lib/mirrorable.rb
+++ lib/mirrorable.rb
@@ -5,8 +5,8 @@ class Mirrorable
 
   def initialize(origin, reflection)
     @broken = false
-    @origin = origin
-    @reflection = reflection
+    set_origin(origin)
+    set_reflection(reflection)
     unless(broken? || File.exist?(origin))
       if(File.exist?(reflection))
         puts "Warning: Origin file #{origin} does not exist but reflection does. Copying from reflection."
@@ -24,4 +24,36 @@ class Mirrorable
   end
 
   alias_method(:broken?, :broken)
+
+  private
+
+  def set_origin(origin)
+    @origin = origin
+    unless(File.readable?(origin) && File.writable?(origin))
+      if(File.exist?(origin))
+        puts "Error: Read and/or write permission denied for origin file #{origin}. Cannot mirror file."
+        @broken = true
+      else
+        unless(File.writable?(origin.split('/')[0..-2].join('/')))
+          puts "Error: Origin file #{origin} does not exist and cannot be created. Cannot mirror file."
+          @broken = true
+        end
+      end
+    end
+  end
+
+  def set_reflection(reflection)
+    @reflection = reflection
+    unless(File.readable?(reflection) && File.writable?(reflection))
+      if(File.exist?(reflection))
+        puts "Error: Read and/or write permission denied for reflection file #{reflection}. Cannot mirror file."
+        @broken = true
+      else
+        unless(File.writable?(reflection.split('/')[0..-2].join('/')))
+          puts "Error: Reflection file #{reflection} does not exist and cannot be created. Cannot mirror file."
+          @broken = true
+        end
+      end
+    end
+  end
 end
